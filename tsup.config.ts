24fd60d1a220c48c0b3cfbbcd343150c8b901255
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-17 18:37:19 +0100

    fix: remove unused dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
+  entry: ["./src/index.ts", "./src/configs/*.ts"],
   format: ["esm", "cjs"],
   clean: true,
   outExtension(ctx) {
