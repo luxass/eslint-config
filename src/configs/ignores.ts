db6b9ff7fe61bcc767f80f425e0a2750590302b4
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-12-26 09:27:03 +0100

    refactor(ignores): inlined glob

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,10 +1,43 @@
 import type { FlatConfigItem } from "../types";
-import { GLOB_EXCLUDE } from "../globs";
 
 export function ignores(): FlatConfigItem[] {
   return [
     {
-      ignores: GLOB_EXCLUDE,
+      ignores: [
+        "**/node_modules",
+        "**/dist",
+        "**/out",
+        "**/build",
+        "**/package-lock.json",
+        "**/yarn.lock",
+        "**/pnpm-lock.yaml",
+        "**/bun.lockb",
+
+        "**/output",
+        "**/coverage",
+        "**/temp",
+        "**/.temp",
+        "**/tmp",
+        "**/.tmp",
+        "**/.history",
+        "**/.vitepress/cache",
+        "**/.nuxt",
+        "**/.next",
+        "**/.vercel",
+        "**/.changeset",
+        "**/.idea",
+        "**/.cache",
+        "**/.output",
+        "**/.vite-inspect",
+        "**/.astro",
+
+        "**/CHANGELOG*.md",
+        "**/*.min.*",
+        "**/LICENSE*",
+        "**/__snapshots__",
+        "**/auto-import?(s).d.ts",
+        "**/components.d.ts",
+      ],
     },
   ];
 }
