0e743093d48951a34898aa36581050a37e04f3ea
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-04-13 07:31:16 +0200

    feat: support typeaware in solid config

--- src/configs/solid.ts
+++ src/configs/solid.ts
@@ -1,6 +1,6 @@
 import { GLOB_JSX, GLOB_TSX } from '../globs'
 import type { TypedFlatConfigItem } from '../types'
-import { ensure, interop } from '../utils'
+import { ensure, interop, toArray } from '../utils'
 
 export interface SolidOptions {
   /**
@@ -14,6 +14,12 @@ export interface SolidOptions {
    * @default true
    */
   typescript?: boolean
+
+  /**
+   * When this options is provided, type aware rules will be enabled.
+   * @see https://typescript-eslint.io/linting/typed-linting/
+   */
+  tsconfigPath?: string | string[]
 
   /**
    * Glob patterns for JSX & TSX files.
@@ -35,10 +41,17 @@ export async function solid(options: SolidOptions = {}): Promise<TypedFlatConfig
     'eslint-plugin-solid',
   ])
 
+  const tsconfigPath = options?.tsconfigPath
+    ? toArray(options.tsconfigPath)
+    : undefined
+  const isTypeAware = !!tsconfigPath
+
   const [
     pluginSolid,
+    parserTs,
   ] = await Promise.all([
     interop(import('eslint-plugin-solid')),
+    interop(import('@typescript-eslint/parser')),
   ] as const)
 
   return [
@@ -52,42 +65,43 @@ export async function solid(options: SolidOptions = {}): Promise<TypedFlatConfig
       name: 'luxass/solid/rules',
       files,
       languageOptions: {
+        parser: parserTs,
         parserOptions: {
           ecmaFeatures: {
             jsx: true,
           },
+          ...(isTypeAware ? { project: tsconfigPath } : {}),
         },
         sourceType: 'module',
       },
       rules: {
-        // solid recommended rules
         // reactivity
         'solid/components-return-once': 'warn',
-        'solid/event-handlers': 'warn',
+        'solid/event-handlers': ['error', {
+          // if true, don't warn on ambiguously named event handlers like `onclick` or `onchange`
+          ignoreCase: false,
+          // if true, warn when spreading event handlers onto JSX. Enable for Solid < v1.6.
+          warnOnSpread: false,
+        }],
+
         // these rules are mostly style suggestions
-        'solid/imports': 'warn',
+        'solid/imports': 'error',
         // identifier usage is important
         'solid/jsx-no-duplicate-props': 'error',
         'solid/jsx-no-script-url': 'error',
         'solid/jsx-no-undef': 'error',
         'solid/jsx-uses-vars': 'error',
-        'solid/no-array-handlers': 'off',
         'solid/no-destructure': 'error',
+
         // security problems
-        'solid/no-innerhtml': 'error',
-        // only necessary for resource-constrained environments
-        'solid/no-proxy-apis': 'off',
-        'solid/no-react-deps': 'warn',
-        'solid/no-react-specific-props': 'warn',
+        'solid/no-innerhtml': ['error', { allowStatic: true }],
+        'solid/no-react-deps': 'error',
+        'solid/no-react-specific-props': 'error',
         'solid/no-unknown-namespaces': 'error',
-        // deprecated
-        'solid/prefer-classlist': 'off',
         'solid/prefer-for': 'error',
-        // handled by Solid compiler, opt-in style suggestion
-        'solid/prefer-show': 'off',
         'solid/reactivity': 'warn',
-        'solid/self-closing-comp': 'warn',
-        'solid/style-prop': 'warn',
+        'solid/self-closing-comp': 'error',
+        'solid/style-prop': ['error', { styleProps: ['style', 'css'] }],
 
         ...typescript
           ? {
