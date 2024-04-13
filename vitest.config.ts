bdb364d6dfdee1691e79bf9b67cddbf419b98b31
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-04-13 11:22:46 +0200

    chore: exclude test files inside fixtures

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
     setupFiles: [
       './tests/setup.ts',
     ],
+    exclude: [
+      './tests/fixtures/**/*',
+
+      // we need to also include the default in exclude
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+    ],
   },
 })
