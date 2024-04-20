16d3c8fe0285368593f8e8eef02e826a702605e4
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-04-20 06:37:54 +0200

    refactor!: use eslint-react

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,7 +1,7 @@
 import { isPackageExists } from 'local-pkg'
-import { GLOB_JSX, GLOB_TSX } from '../globs'
+import { GLOB_JSX, GLOB_TS, GLOB_TSX } from '../globs'
 import type { TypedFlatConfigItem } from '../types'
-import { ensure, interop } from '../utils'
+import { ensure, interop, toArray } from '../utils'
 
 export interface ReactOptions {
   /**
@@ -10,28 +10,10 @@ export interface ReactOptions {
   overrides?: TypedFlatConfigItem['rules']
 
   /**
-   * Enable TypeScript support.
-   *
-   * @default true
+   * When this options is provided, type aware rules will be enabled.
+   * @see https://typescript-eslint.io/linting/typed-linting/
    */
-  typescript?: boolean
-
-  /**
-   * Enable JSX A11y support.
-   *
-   * @default false
-   */
-  a11y?: boolean
-
-  /**
-   * Enable React Refresh support.
-   *
-   * NOTE:
-   * If you are using NextJS, you should disable this option.
-   *
-   * @default true
-   */
-  refresh?: boolean
+  tsconfigPath?: string | string[]
 
   /**
    * Glob patterns for JSX & TSX files.
@@ -42,301 +24,141 @@ export interface ReactOptions {
   files?: string[]
 }
 
+// react refresh
+const ReactRefreshAllowConstantExportPackages = [
+  'vite',
+]
+
 export async function react(options: ReactOptions = {}): Promise<TypedFlatConfigItem[]> {
   const {
-    a11y = false,
-    files = [GLOB_JSX, GLOB_TSX],
+    files = [GLOB_TS, GLOB_TSX],
     overrides = {},
-    refresh = true,
-    typescript = true,
   } = options
 
   await ensure([
-    'eslint-plugin-react',
+    '@eslint-react/eslint-plugin',
     'eslint-plugin-react-hooks',
-    ...(refresh ? ['eslint-plugin-react-refresh'] : []),
-    ...(a11y ? ['eslint-plugin-jsx-a11y'] : []),
+    'eslint-plugin-react-refresh',
   ])
+
+  const tsconfigPath = options?.tsconfigPath
+    ? toArray(options.tsconfigPath)
+    : undefined
+  const isTypeAware = !!tsconfigPath
 
   const [
     pluginReact,
     pluginReactHooks,
     pluginReactRefresh,
-    pluginA11y,
+    parserTs,
   ] = await Promise.all([
-    interop(import('eslint-plugin-react')),
+    interop(import('@eslint-react/eslint-plugin')),
     interop(import('eslint-plugin-react-hooks')),
-    ...(refresh ? [interop(import('eslint-plugin-react-refresh'))] : []),
-    ...(a11y ? [interop(import('eslint-plugin-jsx-a11y'))] : []),
+    interop(import('eslint-plugin-react-refresh')),
+    interop(import('@typescript-eslint/parser')),
   ] as const)
 
-  const isAllowConstantExport = isPackageExists('vite')
+  const isAllowConstantExport = ReactRefreshAllowConstantExportPackages.some(
+    (i) => isPackageExists(i),
+  )
+
+  const plugins = pluginReact.configs.all.plugins
 
   return [
     {
       name: 'luxass/react/setup',
       plugins: {
-        'react': pluginReact,
+        'react': plugins['@eslint-react'],
+        'react-dom': plugins['@eslint-react/dom'],
         'react-hooks': pluginReactHooks,
-
-        ...(refresh ? { 'react-refresh': pluginReactRefresh } : {}),
-        ...(a11y ? { 'jsx-a11y': pluginA11y } : {}),
+        'react-hooks-extra': plugins['@eslint-react/hooks-extra'],
+        'react-naming-convention': plugins['@eslint-react/naming-convention'],
+        'react-refresh': pluginReactRefresh,
       },
     },
     {
       name: 'luxass/react/rules',
       files,
       languageOptions: {
+        parser: parserTs,
         parserOptions: {
           ecmaFeatures: {
             jsx: true,
           },
+          ...isTypeAware ? { project: tsconfigPath } : {},
         },
+        sourceType: 'module',
       },
       rules: {
-        ...(a11y
-          ? {
-            // recommended rules for jsx-a11y
-              'jsx-a11y/alt-text': 'error',
-              'jsx-a11y/anchor-ambiguous-text': 'off',
-
-              'jsx-a11y/anchor-has-content': 'error',
-
-              'jsx-a11y/anchor-is-valid': 'error',
-              'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
-              'jsx-a11y/aria-props': 'error',
-              'jsx-a11y/aria-proptypes': 'error',
-              'jsx-a11y/aria-role': 'error',
-              'jsx-a11y/aria-unsupported-elements': 'error',
-              'jsx-a11y/autocomplete-valid': 'error',
-              'jsx-a11y/click-events-have-key-events': 'error',
-              'jsx-a11y/control-has-associated-label': [
-                'off',
-                {
-                  ignoreElements: [
-                    'audio',
-                    'canvas',
-                    'embed',
-                    'input',
-                    'textarea',
-                    'tr',
-                    'video',
-                  ],
-                  ignoreRoles: [
-                    'grid',
-                    'listbox',
-                    'menu',
-                    'menubar',
-                    'radiogroup',
-                    'row',
-                    'tablist',
-                    'toolbar',
-                    'tree',
-                    'treegrid',
-                  ],
-                  includeRoles: [
-                    'alert',
-                    'dialog',
-                  ],
-                },
-              ],
-              'jsx-a11y/heading-has-content': 'error',
-              'jsx-a11y/html-has-lang': 'error',
-              'jsx-a11y/iframe-has-title': 'error',
-              'jsx-a11y/img-redundant-alt': 'error',
-              'jsx-a11y/interactive-supports-focus': [
-                'error',
-                {
-                  tabbable: [
-                    'button',
-                    'checkbox',
-                    'link',
-                    'searchbox',
-                    'spinbutton',
-                    'switch',
-                    'textbox',
-                  ],
-                },
-              ],
-              'jsx-a11y/label-has-associated-control': 'error',
-              'jsx-a11y/label-has-for': 'off',
-              'jsx-a11y/media-has-caption': 'error',
-              'jsx-a11y/mouse-events-have-key-events': 'error',
-              'jsx-a11y/no-access-key': 'error',
-              'jsx-a11y/no-autofocus': 'error',
-              'jsx-a11y/no-distracting-elements': 'error',
-              'jsx-a11y/no-interactive-element-to-noninteractive-role': [
-                'error',
-                {
-                  canvas: [
-                    'img',
-                  ],
-                  tr: [
-                    'none',
-                    'presentation',
-                  ],
-                },
-              ],
-              'jsx-a11y/no-noninteractive-element-interactions': [
-                'error',
-                {
-                  alert: [
-                    'onKeyUp',
-                    'onKeyDown',
-                    'onKeyPress',
-                  ],
-                  body: [
-                    'onError',
-                    'onLoad',
-                  ],
-                  dialog: [
-                    'onKeyUp',
-                    'onKeyDown',
-                    'onKeyPress',
-                  ],
-                  handlers: [
-                    'onClick',
-                    'onError',
-                    'onLoad',
-                    'onMouseDown',
-                    'onMouseUp',
-                    'onKeyPress',
-                    'onKeyDown',
-                    'onKeyUp',
-                  ],
-                  iframe: [
-                    'onError',
-                    'onLoad',
-                  ],
-                  img: [
-                    'onError',
-                    'onLoad',
-                  ],
-                },
-              ],
-              'jsx-a11y/no-noninteractive-element-to-interactive-role': [
-                'error',
-                {
-                  fieldset: [
-                    'radiogroup',
-                    'presentation',
-                  ],
-                  li: [
-                    'menuitem',
-                    'option',
-                    'row',
-                    'tab',
-                    'treeitem',
-                  ],
-                  ol: [
-                    'listbox',
-                    'menu',
-                    'menubar',
-                    'radiogroup',
-                    'tablist',
-                    'tree',
-                    'treegrid',
-                  ],
-                  table: [
-                    'grid',
-                  ],
-                  td: [
-                    'gridcell',
-                  ],
-                  ul: [
-                    'listbox',
-                    'menu',
-                    'menubar',
-                    'radiogroup',
-                    'tablist',
-                    'tree',
-                    'treegrid',
-                  ],
-                },
-              ],
-              'jsx-a11y/no-noninteractive-tabindex': [
-                'error',
-                {
-                  allowExpressionValues: true,
-                  roles: [
-                    'tabpanel',
-                  ],
-                  tags: [],
-                },
-              ],
-              'jsx-a11y/no-redundant-roles': 'error',
-              'jsx-a11y/no-static-element-interactions': [
-                'error',
-                {
-                  allowExpressionValues: true,
-                  handlers: [
-                    'onClick',
-                    'onMouseDown',
-                    'onMouseUp',
-                    'onKeyPress',
-                    'onKeyDown',
-                    'onKeyUp',
-                  ],
-                },
-              ],
-              'jsx-a11y/role-has-required-aria-props': 'error',
-              'jsx-a11y/role-supports-aria-props': 'error',
-              'jsx-a11y/scope': 'error',
-              'jsx-a11y/tabindex-no-positive': 'error',
-            }
-          : {}),
-
-        // recommended rules react
-        'react/display-name': 'error',
-        'react/jsx-key': 'error',
-
-        'react/jsx-no-comment-textnodes': 'error',
-
-        'react/jsx-no-duplicate-props': 'error',
-        'react/jsx-no-target-blank': 'error',
-        'react/jsx-no-undef': 'error',
-        'react/jsx-uses-react': 'error',
-        'react/jsx-uses-vars': 'error',
-        'react/no-children-prop': 'error',
-        'react/no-danger-with-children': 'error',
-        'react/no-deprecated': 'error',
-        'react/no-direct-mutation-state': 'error',
-        'react/no-find-dom-node': 'error',
-        'react/no-is-mounted': 'error',
-        'react/no-render-return-value': 'error',
-        'react/no-string-refs': 'error',
-        'react/no-unescaped-entities': 'error',
-        'react/no-unknown-property': 'error',
-        'react/no-unsafe': 'off',
-        'react/prop-types': 'error',
-        'react/react-in-jsx-scope': 'off',
-        'react/require-render-return': 'error',
+        // recommended rules from @eslint-react/dom
+        'react-dom/no-children-in-void-dom-elements': 'warn',
+        'react-dom/no-dangerously-set-innerhtml': 'warn',
+        'react-dom/no-dangerously-set-innerhtml-with-children': 'error',
+        'react-dom/no-find-dom-node': 'error',
+        'react-dom/no-missing-button-type': 'warn',
+        'react-dom/no-missing-iframe-sandbox': 'warn',
+        'react-dom/no-namespace': 'error',
+        'react-dom/no-render-return-value': 'error',
+        'react-dom/no-script-url': 'warn',
+        'react-dom/no-unsafe-iframe-sandbox': 'warn',
+        'react-dom/no-unsafe-target-blank': 'warn',
 
         // recommended rules react-hooks
         'react-hooks/exhaustive-deps': 'warn',
         'react-hooks/rules-of-hooks': 'error',
 
         // react refresh
-        ...(refresh
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: isAllowConstantExport },
+        ],
+
+        // recommended rules from @eslint-react
+        'react/ensure-forward-ref-using-ref': 'warn',
+        'react/no-access-state-in-setstate': 'error',
+        'react/no-array-index-key': 'warn',
+        'react/no-children-count': 'warn',
+        'react/no-children-for-each': 'warn',
+        'react/no-children-map': 'warn',
+        'react/no-children-only': 'warn',
+        'react/no-children-prop': 'warn',
+        'react/no-children-to-array': 'warn',
+        'react/no-clone-element': 'warn',
+        'react/no-comment-textnodes': 'warn',
+        'react/no-component-will-mount': 'error',
+        'react/no-component-will-receive-props': 'error',
+        'react/no-component-will-update': 'error',
+        'react/no-create-ref': 'error',
+        'react/no-direct-mutation-state': 'error',
+        'react/no-duplicate-key': 'error',
+        'react/no-implicit-key': 'error',
+        'react/no-missing-key': 'error',
+        'react/no-nested-components': 'warn',
+        'react/no-redundant-should-component-update': 'error',
+        'react/no-set-state-in-component-did-mount': 'warn',
+        'react/no-set-state-in-component-did-update': 'warn',
+        'react/no-set-state-in-component-will-update': 'warn',
+        'react/no-string-refs': 'error',
+        'react/no-unsafe-component-will-mount': 'warn',
+        'react/no-unsafe-component-will-receive-props': 'warn',
+        'react/no-unsafe-component-will-update': 'warn',
+        'react/no-unstable-context-value': 'error',
+        'react/no-unstable-default-props': 'error',
+        'react/no-unused-class-component-members': 'warn',
+        'react/no-unused-state': 'warn',
+        'react/no-useless-fragment': 'warn',
+        'react/prefer-destructuring-assignment': 'warn',
+        'react/prefer-shorthand-boolean': 'warn',
+        'react/prefer-shorthand-fragment': 'warn',
+
+        ...(isTypeAware
           ? {
-              'react-refresh/only-export-components': ['warn', { allowConstantExport: isAllowConstantExport }],
+              'react/no-leaked-conditional-rendering': 'warn',
             }
           : {}),
 
-        ...typescript
-          ? {
-              'react/jsx-no-undef': 'off',
-              'react/prop-type': 'off',
-            }
-          : {},
-
         // overrides
         ...overrides,
-      },
-      settings: {
-        react: {
-          version: 'detect',
-        },
       },
     },
   ]
