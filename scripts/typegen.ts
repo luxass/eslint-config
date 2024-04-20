c33f4ce649877c5d26c7460fe4c5d3e47c7e5218
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-04-20 06:29:23 +0200

    fix: update typegen

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -65,8 +65,15 @@ const configs = await combine(
   svelte(),
 )
 
-const dts = await flatConfigsToRulesDTS(configs, {
+const configNames = configs.map((i) => i.name).filter(Boolean) as string[]
+
+let dts = await flatConfigsToRulesDTS(configs, {
   includeAugmentation: false,
 })
+
+dts += `
+// Names of all the configs
+export type ConfigNames = ${configNames.map((i) => `'${i}'`).join(' | ')}
+`
 
 await fs.writeFile('src/typegen.d.ts', dts)
