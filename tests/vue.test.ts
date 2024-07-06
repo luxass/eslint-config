698de1e4fb038e306090e26bda080d7ffafac817
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-06 09:01:08 +0200

    test(vue): add more vue tests

--- tests/vue.test.ts
+++ tests/vue.test.ts
@@ -3,7 +3,7 @@ import { join } from "node:path";
 import { createEslint } from "./utils/eslint";
 import { getSnapshotPath } from "./utils/snapshot";
 
-describe("vue", async () => {
+describe("vue config", async () => {
   const [linter, fixer] = await createEslint({
     vue: true,
   });
@@ -12,61 +12,123 @@ describe("vue", async () => {
     new URL("./fixtures/vue", import.meta.url),
   );
 
-  it("should be able to lint vue when enabled", async () => {
-    const [
-      [lintResults],
-      [fixedResults],
-    ] = await Promise.all([
-      linter.lintFiles(join(baseUrl, "index-ts.vue")),
-      fixer.lintFiles(join(baseUrl, "index-ts.vue")),
-    ]);
+  describe("typescript", async () => {
+    it("should work with vue", async () => {
+      const [
+        [lintResults],
+        [fixedResults],
+      ] = await Promise.all([
+        linter.lintFiles(join(baseUrl, "index-ts.vue")),
+        fixer.lintFiles(join(baseUrl, "index-ts.vue")),
+      ]);
 
-    [
-      expect.objectContaining({
-        ruleId: "style/quotes",
-        severity: 2,
-        messageId: "wrongQuotes",
-      }),
-    ].forEach((matcher) => {
-      expect(lintResults.messages).toEqual(
-        expect.arrayContaining([matcher]),
-      );
+      [
+        expect.objectContaining({
+          ruleId: "style/quotes",
+          severity: 2,
+          messageId: "wrongQuotes",
+        }),
+      ].forEach((matcher) => {
+        expect(lintResults.messages).toEqual(
+          expect.arrayContaining([matcher]),
+        );
+      });
+
+      const [snapshotPath] = await getSnapshotPath(baseUrl, "typescript/index-ts.linted.vue", fixedResults.output);
+
+      expect(fixedResults.messages).toEqual([]);
+      expect.soft(fixedResults.output).toMatchFileSnapshot(snapshotPath);
     });
 
-    const [snapshotPath] = await getSnapshotPath(baseUrl, "index-ts.linted.vue", fixedResults.output);
+    it("ignore vue files when disabled", async () => {
+      const [linter, fixer] = await createEslint({
+        vue: false,
+      });
 
-    expect(fixedResults.messages).toEqual([]);
-    expect.soft(fixedResults.output).toMatchFileSnapshot(snapshotPath);
+      const [
+        [lintResults],
+        [fixedResults],
+      ] = await Promise.all([
+        linter.lintFiles(join(baseUrl, "index-ts.vue")),
+        fixer.lintFiles(join(baseUrl, "index-ts.vue")),
+      ]);
+
+      expect(lintResults.messages).toEqual([
+        expect.objectContaining({
+          fatal: false,
+          severity: 1,
+        }),
+      ]);
+
+      expect(fixedResults.messages).toEqual([
+        expect.objectContaining({
+          fatal: false,
+          severity: 1,
+        }),
+      ]);
+
+      const [snapshotPath, snapshotContent] = await getSnapshotPath(baseUrl, "typescript/index-ts.disabled.linted.vue", fixedResults.output);
+      expect.soft(snapshotContent).toMatchFileSnapshot(snapshotPath);
+    });
   });
 
-  it("ignore vue files when disabled", async () => {
-    const [linter, fixer] = await createEslint({
-      vue: false,
+  describe("javascript", async () => {
+    it("should work with vue", async () => {
+      const [
+        [lintResults],
+        [fixedResults],
+      ] = await Promise.all([
+        linter.lintFiles(join(baseUrl, "index-js.vue")),
+        fixer.lintFiles(join(baseUrl, "index-js.vue")),
+      ]);
+
+      [
+        expect.objectContaining({
+          ruleId: "style/quotes",
+          severity: 2,
+          messageId: "wrongQuotes",
+        }),
+      ].forEach((matcher) => {
+        expect(lintResults.messages).toEqual(
+          expect.arrayContaining([matcher]),
+        );
+      });
+
+      const [snapshotPath] = await getSnapshotPath(baseUrl, "javascript/index-js.linted.vue", fixedResults.output);
+
+      expect(fixedResults.messages).toEqual([]);
+      expect.soft(fixedResults.output).toMatchFileSnapshot(snapshotPath);
     });
 
-    const [
-      [lintResults],
-      [fixedResults],
-    ] = await Promise.all([
-      linter.lintFiles(join(baseUrl, "index-js.vue")),
-      fixer.lintFiles(join(baseUrl, "index-js.vue")),
-    ]);
+    it("ignore vue files when disabled", async () => {
+      const [linter, fixer] = await createEslint({
+        vue: false,
+      });
 
-    expect(lintResults.messages).toEqual([
-      expect.objectContaining({
-        fatal: false,
-        severity: 1,
-      }),
-    ]);
+      const [
+        [lintResults],
+        [fixedResults],
+      ] = await Promise.all([
+        linter.lintFiles(join(baseUrl, "index-js.vue")),
+        fixer.lintFiles(join(baseUrl, "index-js.vue")),
+      ]);
 
-    expect(fixedResults.messages).toEqual([
-      expect.objectContaining({
-        fatal: false,
-        severity: 1,
-      }),
-    ]);
+      expect(lintResults.messages).toEqual([
+        expect.objectContaining({
+          fatal: false,
+          severity: 1,
+        }),
+      ]);
 
-    const [snapshotPath, snapshotContent] = await getSnapshotPath(baseUrl, "index-disabled.linted.vue", fixedResults.output);
-    expect.soft(snapshotContent).toMatchFileSnapshot(snapshotPath);
+      expect(fixedResults.messages).toEqual([
+        expect.objectContaining({
+          fatal: false,
+          severity: 1,
+        }),
+      ]);
+
+      const [snapshotPath, snapshotContent] = await getSnapshotPath(baseUrl, "javascript/index-js.disabled.linted.vue", fixedResults.output);
+      expect.soft(snapshotContent).toMatchFileSnapshot(snapshotPath);
+    });
   });
 });
