1f10b44f9155a80d50888dc8e40f873618a3cd4e
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-28 06:56:29 +0200

    refactor(markdown): add new rules and remove old rules

--- src/configs/markdown.ts
+++ src/configs/markdown.ts
@@ -83,41 +83,27 @@ export async function markdown(
 
         "no-alert": "off",
         "no-console": "off",
+        "no-labels": "off",
+        "no-lone-blocks": "off",
+        "no-restricted-syntax": "off",
         "no-undef": "off",
         "no-unused-expressions": "off",
-
+        "no-unused-labels": "off",
         "no-unused-vars": "off",
 
         "node/prefer-global/process": "off",
         "style/comma-dangle": "off",
 
         "style/eol-last": "off",
-        // Type aware rules
-        "ts/await-thenable": "off",
         "ts/consistent-type-imports": "off",
-        "ts/dot-notation": "off",
-        "ts/no-floating-promises": "off",
-        "ts/no-for-in-array": "off",
-        "ts/no-implied-eval": "off",
-        "ts/no-misused-promises": "off",
-
         "ts/no-namespace": "off",
         "ts/no-redeclare": "off",
         "ts/no-require-imports": "off",
-
-        "ts/no-throw-literal": "off",
-        "ts/no-unnecessary-type-assertion": "off",
-        "ts/no-unsafe-argument": "off",
-        "ts/no-unsafe-assignment": "off",
-        "ts/no-unsafe-call": "off",
-        "ts/no-unsafe-member-access": "off",
-        "ts/no-unsafe-return": "off",
+        "ts/no-unused-expressions": "off",
         "ts/no-unused-vars": "off",
         "ts/no-use-before-define": "off",
         "ts/no-var-requires": "off",
-        "ts/restrict-plus-operands": "off",
-        "ts/restrict-template-expressions": "off",
-        "ts/unbound-method": "off",
+
         "unicode-bom": "off",
         "unused-imports/no-unused-imports": "off",
         "unused-imports/no-unused-vars": "off",
