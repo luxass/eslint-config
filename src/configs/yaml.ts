6bf21cb184463507f5d9d36c05c23ea4e67bf1d2
Lucas <lucasnrgaard@gmail.com>
2025-03-15 16:17:47 +0100

    feat(yaml): sort pnpm workspace

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -92,5 +92,41 @@ export async function yaml(
         ...overrides,
       },
     },
+    {
+      files: ["pnpm-workspace.yaml"],
+      name: "luxass/yaml/pnpm-workspace",
+      rules: {
+        "yaml/sort-keys": [
+          "error",
+          {
+            order: [
+              "packages",
+              "overrides",
+              "patchedDependencies",
+              "hoistPattern",
+              "catalog",
+              "catalogs",
+
+              "allowedDeprecatedVersions",
+              "allowNonAppliedPatches",
+              "configDependencies",
+              "ignoredBuiltDependencies",
+              "ignoredOptionalDependencies",
+              "neverBuiltDependencies",
+              "onlyBuiltDependencies",
+              "onlyBuiltDependenciesFile",
+              "packageExtensions",
+              "peerDependencyRules",
+              "supportedArchitectures",
+            ],
+            pathPattern: "^$",
+          },
+          {
+            order: { type: "asc" },
+            pathPattern: ".*",
+          },
+        ],
+      },
+    },
   ];
 }
