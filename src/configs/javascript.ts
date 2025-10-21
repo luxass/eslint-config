da951ff8ae9dcb18f4d974ce4be23a6c1d35c7f4
Lucas <lucasnrgaard@gmail.com>
2025-10-21 10:39:57 +0200

    feat(javascript): update ecmaVersion to 'latest'
    * Changed `ecmaVersion` from 2022 to 'latest' for improved compatibility.
    * Ensures the configuration uses the most recent ECMAScript features.

--- src/configs/javascript.ts
+++ src/configs/javascript.ts
@@ -28,7 +28,7 @@ export async function javascript(
   return [
     {
       languageOptions: {
-        ecmaVersion: 2022,
+        ecmaVersion: 'latest',
         globals: {
           ...globals.browser,
           ...globals.es2021,
@@ -41,7 +41,7 @@ export async function javascript(
           ecmaFeatures: {
             jsx: true,
           },
-          ecmaVersion: 2022,
+          ecmaVersion: 'latest',
           sourceType: "module",
         },
         sourceType: "module",
